2db2fa13791bdcb34dec634db8ed4eb3c97d0ba3
alexpurs1980 <alexpurs@yandex.ru>
2023-09-09 21:31:20 +0300

    Added central block of main weather info and temperature

--- WeatherApp-SwiftUI/ContentView.swift
+++ WeatherApp-SwiftUI/ContentView.swift
@@ -7,12 +7,41 @@
 
 import SwiftUI
 
+var cityName: String = "Moscow, Russia"
+var mainTemperatureInt: Int = 21
+var mainTemperatureString = String(mainTemperatureInt) + "℃"
+
+
 struct ContentView: View {
     var body: some View {
         ZStack{
-            LinearGradient(gradient: Gradient(colors:[.blue, .white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            LinearGradient(gradient: Gradient(colors:[.blue, .white]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(.all)
-                //.grayscale(0.25)
+                
+            //main screen vertical stack with all views
+            VStack {
+                //Upper text - city name
+                Text(cityName)
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                
+                //main temperature and icon screen block in center
+                VStack(spacing: 10) {
+                    Image(systemName: "cloud.sun.fill")
+                        //set multicolour
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    Text(mainTemperatureString)
+                        .font(.system(size: 70, weight: .regular))
+                        .foregroundColor(.white)
+                }
+                Spacer()
+            }
         }
     }
 }
