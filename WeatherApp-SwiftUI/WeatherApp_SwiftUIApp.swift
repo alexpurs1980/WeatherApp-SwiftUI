58b88671fc1ca815fcdd4ddbb3c1970169383327
alexpurs1980 <alexpurs@yandex.ru>
2023-09-09 19:51:09 +0300

    Initial Commit

---
+++ WeatherApp-SwiftUI/WeatherApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_SwiftUIApp.swift
+//  WeatherApp-SwiftUI
+//
+//  Created by Алексей Пурис on 09.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
